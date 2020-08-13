d1d33467fccea45578fea7fae906ba9b5bc5e4ea
Matt Hardy <matthewhardy17@gmail.com>
2020-08-13 19:32:45 +0100

    fixed dose deletion issue

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -19,11 +19,8 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    if @dose.destroy
-      redirect_to cocktail_path
-    else
-      redirect_to cocktail_path
-    end
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail) 
   end
   
   private
